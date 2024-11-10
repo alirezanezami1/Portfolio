7affc492f488d9a583f6a5c27921cdf9ccb0fd80
alireza nezami <nezamialireza1022@gmail.com>
2024-11-10 22:36:19 +0330

    add openMenu ref in script-Header/index.vue

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -1,3 +1,7 @@
+<script setup>
+const openMenu = ref(false)
+</script>
+
 <template>
     <div class="flex items-center justify-between py-2.5 px-4 md:px-20 border-b-[1.5px] border-[#CDCFCD] ">
        
