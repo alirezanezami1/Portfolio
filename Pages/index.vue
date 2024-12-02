7baf2d801ff5f7e8b1870be18f96fc5fae7c8d4b
alireza nezami <nezamialireza1022@gmail.com>
2024-12-02 16:40:03 +0330

    responsive introduction section

--- Pages/index.vue
+++ Pages/index.vue
@@ -36,36 +36,36 @@ const toggleAccordion = function (index) {
         </div>
 
         <!-- //// Introduction -->
-        <div class="flex grid-flow-row grid-cols-3 gap-3 justify-center items-center my-11 mx-20">
+        <div class="flex grid-cols-3 flex-col-reverse lg:flex-row justify-center items-center my-11 mx-20 gap-8 lg:gap-3">
             
-            <div class="flex flex-col justify-center items-center gap-[84px] w-[146px]" dir="ltr">
+            <div class="flex lg:flex-col justify-center items-center gap-[37px] lg:gap-[84px] w-[350px] h-[81px] lg:h-auto lg:w-[146px]" dir="ltr">
     
-                <div class="flex flex-col justify-center items-center gap-1 w-full h-[116px]">
-                    <h1 class="text-[56px] leading-[120%] font-bold">+ 88</h1>
-                    <p class="text-[32px] font-medium leading-[140%] text-txt2">نمونه کار</p>
+                <div class="flex flex-col justify-center items-center gap-1 w-1/3 lg:w-full h-[116px]">
+                    <h1 class="text-[32px] lg:text-[56px] leading-[120%] font-bold">+ 88</h1>
+                    <p class="text-[20px] lg:text-[32px] font-medium leading-[160%] lg:leading-[140%] text-txt2">نمونه کار</p>
                 </div>
     
-                <div class="flex flex-col justify-center items-center gap-1 w-full h-[116px]">
-                    <h1 class="text-[56px] leading-[120%] font-bold">+ 8</h1>
-                    <p class="text-[32px] font-medium leading-[140%] text-txt2">سال سابقه</p>
+                <div class="flex flex-col justify-center items-center gap-1 w-1/3 lg:w-full h-[116px]">
+                    <h1 class="text-[32px] lg:text-[56px] leading-[120%] font-bold">+ 8</h1>
+                    <p class="text-[20px] lg:text-[32px] font-medium leading-[160%] lg:leading-[140%] text-txt2">سال سابقه</p>
                 </div>
     
-                <div class="flex flex-col justify-center items-center gap-1 w-full h-[116px]">
-                    <h1 class="text-[56px] leading-[120%] font-bold">+ 94</h1>
-                    <p class="text-[32px] font-medium leading-[140%] text-txt2">رضایتمندی</p>
+                <div class="flex flex-col justify-center items-center gap-1 w-1/3 lg:w-full h-[116px]">
+                    <h1 class="text-[32px] lg:text-[56px] leading-[120%] font-bold">+ 94</h1>
+                    <p class="text-[20px] lg:text-[32px] font-medium leading-[160%] lg:leading-[140%] text-txt2">رضایتمندی</p>
                 </div>
     
             </div>
     
-            <div class="w-[606.89px] h-[664px]">
-                <img src="../assets/adminPhoto/Main image and text container.png" alt="mainPhoto" class="object-cover w-[606.89px] h-[664px]">
+            <div class="flex justify-center items-center">
+                <img src="../assets/adminPhoto/Main image and text container.png" alt="mainPhoto" class="object-cover w-[328px] lg:w-[606.89px] h-[420px] lg:h-[664px]">
             </div>
     
-            <div class="text-right grid gap-[48px] w-[524px]">
+            <div class="text-center lg:text-right flex flex-col justify-center items-center lg:items-start gap-[48px] w-[524px] max-[768px]:w-[328px]">
                 <div class="flex flex-col gap-[40px]">
                     <p class="text-[16px] leading-[160%] font-normal">سلام  👋 من رقیه رنجبری هستم، یه </p>
-                <h1 class="text-[64px] font-bold leading-[120%]">کارشناس سئو </h1>
-                <p class="text-txt2 text-[16px] leading-[160%] font-normal">سؤالات زیادی توی ذهنت هست که دوست داری جواب دقیقشون رو بدونی؟ 😎 کافیه شماره‌ت رو بذاری تا با هم صحبت کنیم و کسب‌وکارت رو بررسی کنیم. ببینیم که آیا سئو برای رشد و موفقیت کار تو مناسب هست یا نه. 🔍📈</p>
+                <h1 class="text-[44px] lg:text-[64px] font-bold leading-[140%] lg:leading-[120%]">کارشناس سئو </h1>
+                <p class="text-txt2 text-[14px] lg:text-[16px] leading-[160%] font-normal">سؤالات زیادی توی ذهنت هست که دوست داری جواب دقیقشون رو بدونی؟ 😎 کافیه شماره‌ت رو بذاری تا با هم صحبت کنیم و کسب‌وکارت رو بررسی کنیم. ببینیم که آیا سئو برای رشد و موفقیت کار تو مناسب هست یا نه. 🔍📈</p>
                 </div>
                <div>
                 <button class="bg-btn1 p-3 flex text-white justify-center items-center gap-3 rounded-lg">
@@ -334,4 +334,11 @@ const toggleAccordion = function (index) {
   height: min-content !important;
   z-index: -1000 ; 
 }
+
+@media screen and (max-width : 760px) {
+    .background {
+        display: none;
+    }
+}
+
 </style>
