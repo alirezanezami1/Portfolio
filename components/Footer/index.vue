593efaafeec1a594a43d3f1b25748740cc552fa8
alireza nezami <nezamialireza1022@gmail.com>
2024-12-04 16:37:01 +0330

    responsive footer section

--- components/Footer/index.vue
+++ components/Footer/index.vue
@@ -8,17 +8,17 @@ const SocialImg = [
 </script>
 
 <template>
-    <div class="flex flex-col gap-7 justify-center items-center bg-Bg/2 p-20">
-        <div class="flex justify-center items-center gap-16">
-            <div class="flex flex-col gap-[26px] w-[512px]">
+    <div class="flex flex-col gap-7 w-full justify-center items-center bg-Bg/2 p-2 md:p-20">
+        <div class="flex flex-col xl:flex-row justify-center items-center gap-16">
+            <div class="flex flex-col gap-[26px] w-[328px] sm:w-[704px] xl:w-[512px]">
                 <div class="flex flex-col justify-center items-start gap-4">
                     <div class="w-[40px] h-[40px]">
                         <img src="../../assets/adminPhoto/c82378950ce20a11d88ae82cc904617a.png" class="object-cover rounded-full w-full h-full" >
                     </div>
                 
                     <div class="flex flex-col gap-1">
-                        <p class="font-normal text-[16px] leading-[25.6px] text-white">رقیه رنجبری</p>
-                        <p class="font-normal text-[16px] leading-[19.2px] text-txt5">کارشناس سئو و بازاریاب دیجیتال</p>
+                        <p class="font-normal text-[20px] leading-[140%] md:text-[16px] md:leading-[25.6px] text-white">رقیه رنجبری</p>
+                        <p class="font-normal text-[14px] leading-[160%] md:text-[16px] md:leading-[19.2px] text-txt5">کارشناس سئو و بازاریاب دیجیتال</p>
                     </div>
                 </div>
 
@@ -27,10 +27,10 @@ const SocialImg = [
                 </div>
             </div>
 
-            <div class="flex justify-center items-start text-right gap-16 w-[704px] ">
+            <div class="flex flex-col md:flex-row justify-center items-start text-right gap-16 w-[328px] sm:w-full xl:w-[704px]">
                 <div class="flex flex-col justify-start items-start gap-6">
-                    <h4 class="text-[24px] leading-[140%] text-white">لینک های اصلی</h4>
-                    <div class="flex flex-col text-[16px] leading-[160%] gap-1 justify-center items-start text-txt5">
+                    <h4 class="text-[20px] md:text-[24px] leading-[140%] text-white">لینک های اصلی</h4>
+                    <div class="flex flex-row md:flex-col text-[16px] leading-[160%] gap-8 md:gap-1 flex-wrap w-full md:w-auto justify-start md:justify-center items-start text-txt5">
                         <NuxtLink to="/" >صفحه اصلی</NuxtLink>
                         <NuxtLink to="/">خدمات</NuxtLink>
                         <NuxtLink to="/">نمونه کارها</NuxtLink>
@@ -40,8 +40,8 @@ const SocialImg = [
                 </div>
 
                 <div class="flex flex-col justify-start items-start gap-6">
-                    <h4 class="text-[24px] leading-[140%] text-white">خدمات اصلی</h4>
-                    <div class="flex flex-col text-[16px] leading-[160%] gap-1 justify-center items-start text-txt5">
+                    <h4 class="text-[20px] md:text-[24px] leading-[140%] text-white">خدمات اصلی</h4>
+                    <div class="flex flex-row md:flex-col text-[16px] leading-[160%] gap-8 md:gap-1 flex-wrap w-full md:w-auto justify-start md:justify-center items-start text-txt5">
                         <NuxtLink to="/" >انحام پروژه سئو</NuxtLink>
                         <NuxtLink to="/">مشاوره سئو</NuxtLink>
                         <NuxtLink to="/">منتورینگ سئو</NuxtLink>
@@ -72,10 +72,10 @@ const SocialImg = [
 
         </div>
 
-        <div class="flex justify-center items-center gap-4">
+        <div class="flex justify-center items-center gap-4 flex-col-reverse xl:flex-row">
             <div class="flex justify-center items-center gap-4">
             <span><img src="/img/Copyright.png" alt="Copyright"></span>
-            <p class="text-[16px] leading-[160%] text-txt5">کلیه حقوق این وبسایت متعلق به سرکار خانم رقیه رنجبری می باشد</p>
+            <p class="text-[14px] md:text-[16px] leading-[160%] text-txt5">کلیه حقوق این وبسایت متعلق به سرکار خانم رقیه رنجبری می باشد</p>
             </div>
 
             <div>
