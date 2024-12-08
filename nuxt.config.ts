a8d9e8f32ff240483ea84db7fbbbe20fb1eb45ad
alireza nezami <nezamialireza1022@gmail.com>
2024-12-08 22:14:45 +0330

    remove @vee-validate/nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,31 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  modules: ['@formkit/auto-animate/nuxt', '@nuxt/image', 'nuxt-keen-slider', 'nuxt-swiper'],
+  modules: ['@formkit/auto-animate/nuxt', '@nuxt/image', 'nuxt-keen-slider', 'nuxt-swiper',   [
+    '@vee-validate/nuxt',
+    {
+      // disable or enable auto imports
+      autoImports: true,
+      // Use different names for components
+      componentNames: {
+        Form: 'VeeForm',
+        Field: 'VeeField',
+        FieldArray: 'VeeFieldArray',
+        ErrorMessage: 'VeeErrorMessage',
+      },
+    },
+  ],],
+  veeValidate: {
+    // disable or enable auto imports
+    autoImports: true,
+    // Use different names for components
+    componentNames: {
+      Form: 'VeeForm',
+      Field: 'VeeField',
+      FieldArray: 'VeeFieldArray',
+      ErrorMessage: 'VeeErrorMessage',
+    },
+  },
   vite: {
     vue: {
       template: {
