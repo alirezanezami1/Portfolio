e53540c8b34d53edbf0031784ac5149bcfc34dd4
alireza nezami <nezamialireza1022@gmail.com>
2024-12-11 10:34:43 +0330

    fix bug

--- Pages/services/seo.vue
+++ Pages/services/seo.vue
@@ -88,7 +88,7 @@ watch(currentTime, (newTime) => {
         <div class="flex justify-start items-start text-[12px] leading-[160%] gap-2 w-[328px] md:w-full">
             <NuxtLink to="/" class="flex justify-center items-center gap-2 text-txt2 ">صفحه اصلی<IconsLinearGray></IconsLinearGray></NuxtLink>
             <NuxtLink class="flex justify-center items-center gap-2 text-txt2 ">خدمات<IconsLinearGray></IconsLinearGray></NuxtLink>
-            <span>درباره من</span>
+            <span>سئوی سایت</span>
         </div>
 
         <!-- //// بنر  -->
