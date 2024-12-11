91cd66cc1703772470fe76b1a87208fadf2ce61f
alireza nezami <nezamialireza1022@gmail.com>
2024-12-11 10:36:51 +0330

    add link in seo consulting

--- Pages/services/seo-consulting.vue
+++ Pages/services/seo-consulting.vue
@@ -1,3 +1,18 @@
 <template>
-    <div class="flex flex-col justify-center items-center my-10 mx-20"></div>
+    <div class="flex flex-col justify-center items-center my-10 mx-20">
+
+         <!-- //// لینک ها  -->
+         <div class="flex justify-start items-start text-[12px] leading-[160%] gap-2 w-[328px] md:w-full">
+            <NuxtLink to="/" class="flex justify-center items-center gap-2 text-txt2 ">صفحه اصلی<IconsLinearGray></IconsLinearGray></NuxtLink>
+            <NuxtLink class="flex justify-center items-center gap-2 text-txt2 ">خدمات<IconsLinearGray></IconsLinearGray></NuxtLink>
+            <span>مشاوره سئو</span>
+        </div>
+
+         <!-- //// بنر  -->
+         <div class="flex justify-center items-center w-[328px] md:w-full my-10">
+            <img src="/img/seo-consulting/Frame 450.png" alt="banner" class="hidden md:block">
+            <img src="/img/seo-consulting/Frame 450 (1).png" alt="banner" class="flex md:hidden">
+        </div>
+
+    </div>
 </template>
