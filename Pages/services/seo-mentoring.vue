52910ece7d6ba04135d09b0807dd6e0a4a39d5b5
alireza nezami <nezamialireza1022@gmail.com>
2024-12-12 01:32:18 +0330

    fix bug

--- Pages/services/seo-mentoring.vue
+++ Pages/services/seo-mentoring.vue
@@ -229,26 +229,6 @@ watch(currentTime, (newTime) => {
   z-index: -1000 ; 
 }
 
-.pointSection {
-    background-color: rgba(19, 20, 78, .16);
-}
-
-.pointSection .txtPoint::before {
-    content: '';
-    position: absolute;
-    top: 17%;
-    width: 10px !important;
-    height: 10px !important;
-    border-radius: 12px;
-    right: 8px;
-    background-color: #13144E;
-}
-
-@media screen and (min-width : 970px) {
-    .pointSection .txtPoint::before {
-        top: 38%;
-    }
-}
 
 .time-text {
   color: white;
