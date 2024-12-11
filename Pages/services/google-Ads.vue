4a5eebc00c34b7c7f1ba312466a5098bca559a0e
alireza nezami <nezamialireza1022@gmail.com>
2024-12-12 02:19:13 +0330

    add link in Ads

--- Pages/services/google-Ads.vue
+++ Pages/services/google-Ads.vue
@@ -1,3 +1,10 @@
 <template>
-    <div></div>
+    <div class="flex flex-col justify-center items-center my-10 mx-20">
+        <!-- //// لینک ها  -->
+        <div class="flex justify-start items-start text-[12px] leading-[160%] gap-2 w-[328px] md:w-full">
+            <NuxtLink to="/" class="flex justify-center items-center gap-2 text-txt2 ">صفحه اصلی<IconsLinearGray></IconsLinearGray></NuxtLink>
+            <NuxtLink class="flex justify-center items-center gap-2 text-txt2 ">خدمات<IconsLinearGray></IconsLinearGray></NuxtLink>
+            <span>گوگل ادز</span>
+        </div>
+    </div>
 </template>
