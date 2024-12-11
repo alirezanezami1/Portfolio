a01f5fd2726bf297d9e735766492f027f00e2ca7
alireza nezami <nezamialireza1022@gmail.com>
2024-12-12 02:40:50 +0330

    add banner in website design

--- Pages/services/website-design.vue
+++ Pages/services/website-design.vue
@@ -1,10 +1,16 @@
 <template>
     <div class="flex flex-col justify-center items-center my-10 mx-20">
           <!-- //// لینک ها  -->
-          <div class="flex justify-start items-start text-[12px] leading-[160%] gap-2 w-[328px] md:w-full">
+        <div class="flex justify-start items-start text-[12px] leading-[160%] gap-2 w-[328px] md:w-full">
             <NuxtLink to="/" class="flex justify-center items-center gap-2 text-txt2 ">صفحه اصلی<IconsLinearGray></IconsLinearGray></NuxtLink>
             <NuxtLink class="flex justify-center items-center gap-2 text-txt2 ">خدمات<IconsLinearGray></IconsLinearGray></NuxtLink>
             <span>طراحی سایت</span>
+        </div>
+
+        <!-- //// بنر  -->
+        <div class="flex justify-center items-center w-[328px] md:w-full my-10">
+            <img src="/img/seo-ads/Frame 450.png" alt="banner" class="hidden md:block">
+            <img src="/img/seo-ads/Frame 450 (1).png" alt="banner" class="flex md:hidden">
         </div>
     </div>
 </template>
