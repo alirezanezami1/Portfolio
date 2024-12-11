d31f2f8bef1aa2e059d4f1690f4d7c1ea056ff76
alireza nezami <nezamialireza1022@gmail.com>
2024-12-12 01:28:50 +0330

    add hint section

--- Pages/services/seo-strategy.vue
+++ Pages/services/seo-strategy.vue
@@ -1,3 +1,6 @@
+<script setup>
+const hint = ['چرا به تدوین استراتژی سئو نیاز داریم ؟' , 'تعریف تدوین استراتژی سئو' , 'مراحل تدوین استراتژی سئو']
+</script>
 <template>
     <div class="flex flex-col justify-center items-center my-10 mx-20">
         <!-- //// لینک ها  -->
@@ -11,6 +14,14 @@
         <div class="flex justify-center items-center w-[328px] md:w-full my-10">
             <img src="/img/seo-startegy/Frame 450.png" alt="banner" class="hidden md:block">
             <img src="/img/seo-startegy/Frame 450 (1).png" alt="banner" class="flex md:hidden">
+        </div>
+
+         <!-- //// مواردی که در این صفحه به آنها اشاره خواهد شد : -->
+         <div class="flex flex-col justify-start gap-10 items-start rounded-2xl my-20 w-[328px] md:w-full border border-txt3 p-10">
+            <h5 class="text-[20px] leading-[140%] text-txt1 font-bold">مواردی که در این صفحه به آنها اشاره خواهد شد :</h5>
+            <div class="flex flex-col justify-start items-start gap-5">
+                <p v-for="item,index in hint" :key="index" class="text-[14px] md:text-[16px] leading-[160%] text-txt4"><span>{{ index + 1 }}.</span>{{ item }}</p>
+            </div>
         </div>
     </div>
 </template>
