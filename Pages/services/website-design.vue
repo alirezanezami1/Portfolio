1e6dcd54899b9931fe0ee986affbe410650c561a
alireza nezami <nezamialireza1022@gmail.com>
2024-12-12 02:44:03 +0330

    add hint section

--- Pages/services/website-design.vue
+++ Pages/services/website-design.vue
@@ -1,3 +1,7 @@
+<script setup>
+const hint = ['چرایی داشتن طراحی مناسب یک سایت ؟' , 'در زمینه بودجه و هزینه مشکلی دارید ؟' , 'مراحل طراحی سایت که من میتوام در اونها بهت کمک کنم']
+</script>
+
 <template>
     <div class="flex flex-col justify-center items-center my-10 mx-20">
           <!-- //// لینک ها  -->
@@ -12,5 +16,14 @@
             <img src="/img/seo-ads/Frame 450.png" alt="banner" class="hidden md:block">
             <img src="/img/seo-ads/Frame 450 (1).png" alt="banner" class="flex md:hidden">
         </div>
+
+         <!-- //// مواردی که در این صفحه به آنها اشاره خواهد شد : -->
+         <div class="flex flex-col justify-start gap-10 items-start rounded-2xl my-20 w-[328px] md:w-full border border-txt3 p-10">
+            <h5 class="text-[20px] leading-[140%] text-txt1 font-bold">مواردی که در این صفحه به آنها اشاره خواهد شد :</h5>
+            <div class="flex flex-col justify-start items-start gap-5">
+                <p v-for="item,index in hint" :key="index" class="text-[14px] md:text-[16px] leading-[160%] text-txt4"><span>{{ index + 1 }}.</span>{{ item }}</p>
+            </div>
+        </div>
+
     </div>
 </template>
