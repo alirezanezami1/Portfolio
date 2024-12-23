a7f120f9f48024790dadc79411363dee4371bdb8
alireza nezami <nezamialireza1022@gmail.com>
2024-12-23 23:34:54 +0330

    fix bug

--- components/CommentSlider.vue
+++ components/CommentSlider.vue
@@ -23,7 +23,6 @@ import axios from 'axios';
 
 const comments = ref([]);
 
-
 const fetchComments = async () => {
   try {
             const response = await axios.get('http://127.0.0.1:8000/api/comments');
